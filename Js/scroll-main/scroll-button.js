205c1c0cd63678f09ad533674acbddeef860ff14
kseonghee <kseonghee0127@gmail.com>
2024-01-22 17:48:12 +0900

    feat: 수정
    banner 위에 box하나 더 감싸주고 해보기

--- Js/scroll-main/scroll-button.js
+++ Js/scroll-main/scroll-button.js
@@ -1,21 +1,24 @@
 const page = document.querySelector('.category-list');
 
 const leftBtn = document.querySelector('.left-btn');
-leftBtn.onclick = () => {
+leftBtn.addEventListener("click", () => {
     page.scrollTo({ left: 0, behavior: 'smooth'});
-}
-const rightBtn = document.querySelector('.right-btn');
-rightBtn.onclick = () => {
-    page.scrollTo({left : page.scrollWidth, behavior: 'smooth'});
-}
+});
 
-const banner = document.querySelector('.swiper-wrap');
+const rightBtn = document.querySelector('.right-btn');
+rightBtn.addEventListener("click", () => {
+    page.scrollTo({left : page.scrollWidth, behavior: 'smooth'});
+});
+
+
+const bannerPage = document.querySelector('.banner');
 
 const bannerLeftBtn = document.querySelector('.banner-left-btn');
-bannerLeftBtn.onclick = () => {
-    banner.scrollTo({ left: 0, behavior: 'smooth'});
-}
+bannerLeftBtn.addEventListener("click", () => {
+    bannerPage.scrollBy({ left: -bannerPage.offsetWidth, behavior: 'smooth'});
+});
+
 const bannerRightBtn = document.querySelector('.banner-right-btn');
-bannerRightBtn.onclick = () => {
-    banner.scrollTo({ left: , behavior: 'smooth'});
-}
+bannerRightBtn.addEventListener("click", () => {
+    bannerPage.scrollBy({left: bannerPage.offsetWidth, behavior: 'smooth'});
+});
